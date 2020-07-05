0d95a9a97018a40e0b62288b8d8d1a0927c5ae27
Victor Pontis <victor@pont.is>
2020-07-05 17:50:11 -0400

    Update embed logic

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,61 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
     <div className="container">
-
-      <div>
-        Hiiii
-      </div>
+      <div>Hiiii</div>
 
       {/* TODO: think about how we want to handle pages that don't handle scripts well */}
-      <button id="zmurl-checkout-modal-button" type="button">Buy Tickets</button>
+      <button
+        className="zmurl-checkout-modal-button"
+        type="button"
+        data-event-api-id={"evt-paid-event"}
+      >
+        Buy Tickets
+      </button>
 
       <script src="https://embed-scripts.onrender.com/checkout-button.js" />
 
+      <div className={"modal-container"} style={{ display: "none" }}>
+        <div className="modal-body">
+          <p>hi</p>
+          <p>hi</p>
+        </div>
+      </div>
+
       <style jsx global>{`
+        .modal-container {
+          width: 100%;
+          height: 100%;
+
+          overflow: hidden;
+
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          top: 0;
+          left: 0;
+
+          z-index: 2147483647;
+
+          position: fixed;
+          background: rgba(57, 54, 79, 0.8);
+        }
+
+        .modal-body {
+          width: 95%;
+          height: 95%;
+          max-width: 1080px;
+          max-height: 720px;
+
+          background-color: white;
+
+          position: relative;
+
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+        }
+
         html,
         body {
           background-color: blue;
@@ -29,5 +71,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
