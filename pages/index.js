9e1f6688289b90b4fad3548c4d9b86f759b23145
Victor Pontis <victor@pont.is>
2020-07-05 21:52:53 -0400

    temp commit

--- pages/index.js
+++ pages/index.js
@@ -19,48 +19,7 @@ export default function Home() {
 
       <script src={`${EMBED_BASE_URL}/checkout-button.js`} />
 
-      <div className={"modal-container"} style={{ display: "none" }}>
-        <div className="modalhttps://e3a0a934d6fb.ngrok.io">
-          <p>hi</p>
-          <p>hi</p>
-        </div>
-      </div>
-
       <style jsx global>{`
-        .modal-container {
-          width: 100%;
-          height: 100%;
-
-          overflow: hidden;
-
-          display: flex;
-          justify-content: center;
-          align-items: center;
-
-          top: 0;
-          left: 0;
-
-          z-index: 2147483647;
-
-          position: fixed;
-          background: rgba(57, 54, 79, 0.8);
-        }
-
-        .modal-body {
-          width: 95%;
-          height: 95%;
-          max-width: 1080px;
-          max-height: 720px;
-
-          background-color: white;
-
-          position: relative;
-
-          display: flex;
-          flex-direction: column;
-          justify-content: space-between;
-        }
-
         html,
         body {
           background-color: blue;
