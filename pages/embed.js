84294d7285409a6eec35c9727a92ab793f6106bc
Victor Pontis <victor@pont.is>
2023-03-22 13:41:33 -0400

    Add `/embed` page

---
+++ pages/embed.js
@@ -0,0 +1,71 @@
+const isProd = process.env.NODE_ENV === "production"
+const EMBED_BASE_URL =
+    process.env.NODE_ENV === "production"
+        ? "https://embed.lu.ma"
+        : "http://127.0.0.1:5499";
+
+const eventId = isProd ? "evt-QDi9j97FsQl9O2h" : 'evt-free-event'
+
+export default function Home() {
+    return (
+        <div className="container">
+            <h1>Embed iFrame Examples</h1>
+
+            <hr/>
+
+            <h2>Event Embed</h2>
+
+            <iframe
+                src={`${EMBED_BASE_URL}/embed-checkout/${eventId}`}
+                width="600"
+                height="450"
+                frameBorder="0"
+                style={{border: '1px solid #bfcbda88', borderRadius: '4px'}}
+                allowFullScreen=""
+                aria-hidden="false"
+                tabIndex="0"
+            ></iframe>
+
+            <h2>Page Not Allowed</h2>
+
+            <iframe
+                src={`${EMBED_BASE_URL}/signin`}
+                width="600"
+                height="450"
+                frameBorder="0"
+                style={{border: '1px solid #bfcbda88', borderRadius: '4px'}}
+                allowFullScreen=""
+                aria-hidden="false"
+                tabIndex="0"
+            ></iframe>
+
+            <style jsx global>{`
+              html,
+              body {
+                background-color: #f5f8fa;
+                padding: 0;
+                margin: 0;
+                font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                sans-serif;
+                color: #123;
+              }
+
+              * {
+                box-sizing: border-box;
+              }
+
+              .container {
+                max-width: 1000px;
+                padding: 1rem;
+                margin: auto;
+              }
+
+              hr {
+                border-color: #bfcbda44;
+                margin: 2rem 0;
+              }
+            `}</style>
+        </div>
+    );
+}
